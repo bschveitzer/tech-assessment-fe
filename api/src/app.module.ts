2534c1e4d463bffa93a2824612904085f3f04632
Bernardo Souza <bess1101@hotmail.com>
2024-03-27 13:01:43 -0300

    Add dotenv support via configModule and start prisma setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TrialsModule } from './trials/trials.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { TrialsModule } from './trials/trials.module';
         path: join(process.cwd(), 'src/graphql.ts'),
       },
     }),
+    ConfigModule.forRoot(),
     TrialsModule,
   ],
   controllers: [AppController],
