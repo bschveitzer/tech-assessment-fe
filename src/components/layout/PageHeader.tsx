41704bc6c2ac96d9e0567c1dad050253cd0b5b9a
Bernardo Souza <bess1101@hotmail.com>
2024-03-28 16:32:42 -0300

    Create page header component

---
+++ src/components/layout/PageHeader.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const StyledPageHeader = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledTitle = styled.h1`
+  font-size: 32px;
+  font-weight: 400;
+  line-height: 40px;
+`;
+
+interface PageHeaderProps {
+  title: string;
+  action?: React.ReactNode;
+}
+
+export const PageHeader: React.FC<PageHeaderProps> = ({ title, action }) => {
+  return (
+    <StyledPageHeader>
+      <StyledTitle>{title}</StyledTitle>
+      {action}
+    </StyledPageHeader>
+  );
+};
--- src/scenes/trials/index.tsx
+++ src/scenes/trials/index.tsx
@@ -1,16 +1,17 @@
 import { useQuery } from '@apollo/client';
 import React from 'react';
 import { GET_TRIALS } from './graphql/queries';
-import { HeaderNavBar } from '../../components/navigation/HeaderNavBar';
+import { Container } from '../../components/layout/Container';
+import { PageHeader } from '../../components/layout/PageHeader';
 
 export const Trials: React.FC = () => {
   const { data, loading } = useQuery(GET_TRIALS);
 
   console.log(data, loading, process.env.REACT_APP_API_URL);
   return (
-    <div>
+    <Container>
+      <PageHeader title="Trials" />
       Trials 1
-      <HeaderNavBar />
-    </div>
+    </Container>
   );
 };
